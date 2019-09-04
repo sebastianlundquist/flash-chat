1a91972f2ce1b4c7de6ac290eb4291f01bb76ec5
Sebastian Lundqvist <sebastian.lundqvist@combitech.se>
2019-09-04 13:20:59 +0200

    Add routing

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,13 @@ class FlashChat extends StatelessWidget {
         ),
       ),
       home: WelcomeScreen(),
+      initialRoute: WelcomeScreen.id,
+      routes: {
+        ChatScreen.id: (context) => ChatScreen(),
+        LoginScreen.id: (context) => LoginScreen(),
+        RegistrationScreen.id: (context) => RegistrationScreen(),
+        WelcomeScreen.id: (context) => WelcomeScreen(),
+      },
     );
   }
 }
