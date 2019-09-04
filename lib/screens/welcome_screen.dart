80fd028112c65ae0e68c7a6fe7a7121336da2e5d
Sebastian Lundqvist <sebastian.lundqvist@combitech.se>
2019-09-04 14:12:38 +0200

    Animate background color and logo size in initState

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -8,11 +8,43 @@ class WelcomeScreen extends StatefulWidget {
   _WelcomeScreenState createState() => _WelcomeScreenState();
 }
 
-class _WelcomeScreenState extends State<WelcomeScreen> {
+class _WelcomeScreenState extends State<WelcomeScreen>
+    with SingleTickerProviderStateMixin {
+  AnimationController controller;
+  Animation colorAnimation;
+  Animation sizeAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+      duration: Duration(seconds: 1),
+      vsync: this,
+    );
+
+    colorAnimation = ColorTween(begin: Colors.blueGrey, end: Colors.white)
+        .animate(controller);
+
+    sizeAnimation =
+        CurvedAnimation(parent: controller, curve: Curves.decelerate);
+
+    controller.forward();
+
+    controller.addListener(() {
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: colorAnimation.value,
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 24.0),
         child: Column(
@@ -25,7 +57,7 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
                   tag: 'logo',
                   child: Container(
                     child: Image.asset('images/logo.png'),
-                    height: 60.0,
+                    height: sizeAnimation.value * 60,
                   ),
                 ),
                 Text(
